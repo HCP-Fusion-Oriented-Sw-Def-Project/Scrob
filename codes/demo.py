91bdac7b963d358adee9dc95e15ad85368a51067
surenyufuz <69104485+surenyufuz@users.noreply.github.com>
2022-06-11 08:46:22 +0800

    Update demo.py

--- codes/demo.py
+++ codes/demo.py
@@ -108,12 +108,12 @@ def get_scrob_result(file_path, is_static):
     # removed_nodes, changed_nodes, added_nodes = re.get_single_nodes_result()
 
     # 保存结果图
-    # re.save_single_results()
+    # re.save_single_result()
 
     # 列表元素对比结果
     removed_nodes, changed_nodes, added_nodes = re.get_list_nodes_result()
 
     # 保存结果图
-    re.save_list_results()
+    re.save_list_result()
 
     return [removed_nodes, changed_nodes, added_nodes]
